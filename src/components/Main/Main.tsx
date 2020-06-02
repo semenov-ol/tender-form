098681f44629651066f34a61249c614c6ed9a6c8
Oleg <semenov.os16@gmail.com>
2020-06-02 14:59:06 +0300

    feat: Add reducer cases

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 import Text from 'ustudio-ui/components/Text';
 import Flex from 'ustudio-ui/components/Flex';
 import RadioGroup from 'ustudio-ui/components/RadioGroup';
@@ -7,114 +7,144 @@ import { TextInput } from 'ustudio-ui';
 import { TextArea } from 'ustudio-ui';
 import Button from 'ustudio-ui/components/Button';
 import { v4 as uuid } from 'uuid';
+import { css } from 'styled-components';
 
 import { reducer } from '../../context/Reducer';
 import { Data } from '../../types';
+import { Unit } from '../../types';
 
 export const Main = () => {
-  interface State {
-    title: string;
-    description: string;
-    unit: any;
-  }
-  const initialState: State = {
-    title: '',
-    description: '',
-    unit: {},
-  };
-  const [state, dispatch]: any = useReducer<any>(reducer, initialState);
-  const id = uuid();
+	const initialState: Data = {
+		tender: {
+			title: '',
+			description: '',
+			classification: {
+				scheme: 'CPV',
+				id: '',
+				description: ''
+			},
+			lots: [
+			],
+			items: [
+			]
+		}
+	};
+	const [ state, dispatch ]: any = useReducer<any>(reducer, initialState);
+	const id = uuid();
 
-  const tenderData: Data = {
-    tender: {
-      title: state.title,
-      description: state.description,
-      classification: {
-        scheme: 'CPV',
-        id: id,
-        description: state.description,
-      },
-      lots: [
-        {
-          id: id,
-          title: state.title,
-        },
-      ],
-      items: [
-        {
-          id: id,
-          description: state.description,
-          relatedLot: id,
-          classification: {
-            scheme: 'CPV',
-            id: id,
-            description: state.description,
-          },
-          additionalClassification: [
-            {
-              scheme: 'CPV',
-              id: id,
-              description: state.description,
-            },
-          ],
-          quantity: 5,
-          unit: state.unit,
-        },
-      ],
-    },
-  };
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		console.log(state);
+	};
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(tenderData);
-  };
-
-  return (
-    <Flex
-      alignment={{
-        horizontal: 'center',
-        vertical: 'center',
-      }}
-      direction="column"
-    >
-      <Text variant="h3">Create Tender</Text>
-      <form className="form" onSubmit={handleSubmit}>
-        <Text variant="span"> Please fill in the fields: </Text>
-        <Flex direction="row" alignment={{ horizontal: 'space-between' }}>
-          <label>
-            Tender title:
-            <TextInput
-              isRequired
-              name="title"
-              placeholder="Tender Title"
-              value={state.title}
-              onChange={(e) => dispatch({ type: 'ADD_TITLE', payload: e })}
-            />
-          </label>
-        </Flex>
-        <Flex>
-          <label>
-            Tender description
-            <TextArea
-              isRequired
-              name="description"
-              placeholder="Tender description"
-              value={state.description}
-              onChange={(e) => dispatch({ type: 'ADD_DESC', payload: e })}
-            />
-          </label>
-        </Flex>
-        <RadioGroup
-          direction="row"
-          name="unit"
-          onChange={(e) => dispatch({ type: 'ADD_UNIT', payload: e })}
-          options={{
-            '1': { label: 'metre', value: '123' },
-            '2': { label: 'kilo', value: '312' },
+	const fieldRender = () => {
+    const id = '12345'
+		return (
+      <>
+			<TextInput
+				isRequired={false}
+				placeholder="TItle of additional info"
+				onChange={(e) => dispatch({ type: 'ADDITIONAL', payload: {title: e, id: id} })}
+			/>
+      <TextInput
+				isRequired={false}
+				placeholder="description of additional info"
+				onChange={(e) => dispatch({ type: 'ADDITIONAL_DESC', payload: {desc: e, id: id} })}
+			/>
+      <RadioGroup
+					styled={{
+						RadioGroup: css`margin: 30px;`
           }}
-        />
-        <Button>Submit</Button>
-      </form>
-    </Flex>
-  );
+					direction="row"
+					name="unit2"
+					onChange={(e) => dispatch({ type: 'ADD_UNIT2', payload: {title: e, id: id} })}
+					options={{
+						'1': { label: 'metre', value: '123' },
+						'2': { label: 'kilo', value: '312' }
+					}}
+				/>
+    </>
+		);
+  };
+  
+  const id1 = uuid()
+
+	return (
+		<Flex
+			alignment={{
+				horizontal: 'center',
+				vertical: 'center'
+			}}
+			direction="column"
+		>
+			<Text
+				variant="h3"
+				styled={{
+					Text: css`margin: 30px;`
+				}}
+			>
+				Create Tender
+			</Text>
+			<form className="form" onSubmit={handleSubmit}>
+				<Text variant="span"> Please fill in the fields: </Text>
+				<Flex
+					direction="row"
+					alignment={{ horizontal: 'space-between' }}
+					styled={{
+						Flex: css`margin: 30px;`
+					}}
+				>
+					<label>
+						Tender title:
+						<TextInput
+							isRequired
+							name="title"
+							placeholder="Tender Title"
+							value={state.title}
+							onChange={(e) => dispatch({ type: 'ADD_TITLE', payload: e })}
+						/>
+					</label>
+				</Flex>
+				<Flex
+					styled={{
+						Flex: css`margin: 30px;`
+					}}
+				>
+					<label>
+						Tender description
+						<TextArea
+							isRequired
+							name="description"
+							placeholder="Tender description"
+							value={state.description}
+							onChange={(e) => dispatch({ type: 'ADD_DESC', payload: e })}
+						/>
+					</label>
+				</Flex>
+				<RadioGroup
+					styled={{
+						RadioGroup: css`margin: 30px;`
+          }}
+					direction="row"
+					name="unit"
+					onChange={(e) => dispatch({ type: 'ADD_UNIT', payload: {title: e, id: id1} })}
+					options={{
+						'1': { label: 'metre', value: '123' },
+						'2': { label: 'kilo', value: '312' }
+					}}
+				/>
+				{fieldRender()}
+				<Flex
+					alignment={{
+						horizontal: 'end'
+					}}
+					styled={{
+						Flex: css`margin-top: 30px;`
+					}}
+				>
+					<Button>Submit</Button>
+				</Flex>
+			</form>
+		</Flex>
+	);
 };
--- src/context/Reducer.tsx
+++ src/context/Reducer.tsx
@@ -2,22 +2,143 @@ export const reducer = (state: any, action: any) => {
 	switch (action.type) {
 		case 'ADD_TITLE':
 			return {
-				title: action.payload,
-				description: state.description,
-				unit: state.unit
+				tender: {
+					...state.tender,
+					title: action.payload
+				}
 			};
 		case 'ADD_DESC':
 			return {
-				title: state.title,
-				description: action.payload,
-				unit: state.unit
+				tender: {
+					...state.tender,
+					description: action.payload
+				}
 			};
 		case 'ADD_UNIT':
 			return {
-				title: state.title,
-				description: state.description,
-				unit: action.payload
+				tender: {
+					...state.tender,
+					lots: [
+						...state.tender.lots,
+						{
+							id: action.payload.id,
+							title: state.tender.title
+						}
+					],
+					items: [
+						...state.tender.items,
+						{
+							id: action.payload.id,
+							description: state.tender.description,
+							relatedLot: action.payload.id,
+							classification: {
+								scheme: 'CPV',
+								id: '',
+								description: ''
+							},
+							additionalClassification: [
+								{
+									scheme: 'CPV',
+									id: '',
+									description: ''
+								}
+							],
+							quantity: 5,
+							unit: action.payload.title
+						}
+					]
+				}
 			};
+
+		case 'ADDITIONAL':
+			const addItem = state.tender.lots.find((item: any) => item.id === action.payload.id);
+			if (addItem === undefined) {
+				return {
+					tender: {
+						...state.tender,
+						lots: [
+							...state.tender.lots,
+							{
+								id: action.payload.id,
+								title: action.payload.title
+							}
+						],
+						items: [
+							...state.tender.items,
+							{
+								id: action.payload.id,
+								description: '',
+								relatedLot: action.payload.id,
+								classification: {
+									scheme: 'CPV',
+									id: '',
+									description: ''
+								},
+								additionalClassification: [
+									{
+										scheme: 'CPV',
+										id: '',
+										description: ''
+									}
+								],
+								quantity: 5
+							}
+						]
+					}
+				};
+			} else {
+				addItem.title = action.payload.title;
+				return {
+					tender: {
+						...state.tender
+					}
+				};
+			}
+
+		case 'ADDITIONAL_DESC':
+			const item = state.tender.items.find((item: any) => item.id === action.payload.id);
+			if (item === undefined) {
+				return {
+					tender: {
+						...state.tender,
+						items: [
+							...state.tender.items,
+							{
+								id: action.payload.id,
+								description: action.payload.desc,
+								relatedLot: action.payload.id,
+								classification: {
+									scheme: 'CPV',
+									id: '',
+									description: ''
+								},
+								additionalClassification: [
+									{
+										scheme: 'CPV',
+										id: '',
+										description: ''
+									}
+								],
+								quantity: 5
+							}
+						],
+						lots: [
+							...state.tender.lots,
+							{
+								id: action.payload.id,
+								title: ''
+							}
+						]
+					}
+				};
+			} else {
+				item.description = action.payload.desc;
+				return {
+					tender: {
+						...state.tender
+					}
+				};
+			}
 		default:
 			return state;
 	}
